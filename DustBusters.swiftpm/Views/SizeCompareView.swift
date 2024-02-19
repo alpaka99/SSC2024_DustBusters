52ff02db0fa21b68ff2735c112db0d768960120c
Lets <ruvan123@naver.com>
2024-02-19 21:37:27 +0900

    Feature: Create feature for ScalableVIew

--- DustBusters.swiftpm/Views/SizeCompareView.swift
+++ DustBusters.swiftpm/Views/SizeCompareView.swift
@@ -14,27 +14,43 @@ struct SizeCompareView: View {
     @State private var offset: CGFloat = CGFloat.zero
     private var scaleAmount: CGFloat {
         get {
-            if self.offset > 0 {
-                return 0
-            } else {
-                return -(self.offset / 100)
+            switch offset {
+            case let x where ...(-145) ~= x:
+                return pow(10, 145 / 50)
+            case let x where ...0 ~= x:
+                return pow(10, -(self.offset / 50))
+            default:
+                return CGFloat.one
             }
         }
     }
-    
-    let items: [String] = ["Basket ball", "Apple",  "Hair", "MicroDust", "Dummy"]
+
     
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 500) {
-                ForEach(self.items, id: \.self) { item in
-                    Text(String(describing: item))
-                        .frame(maxWidth: 400)
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .center, spacing: 50) {
+                ForEach(Constants.scalableObjects, id: \.self) { item in
+                    Image(item.imageName)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: item.initialSize)
+                        .opacity(item.opacity)
                         .padding(20)
+                        .offset(x: 0.2)
                         .scaleEffect(scaleAmount)
+//                        .onChange(of: scaleAmount) { _ in
+//                            print("\(item.imageName): \(scaleAmount) x \(item.initialSize) = \(scaleAmount * item.initialSize) / offset: \(offset)")
+//                            if item.imageName == Constants.basketBallImageName {
+//                                print(String(repeating: "=", count: 40))
+//                            }
+//                        }
+                        .onTapGesture {
+                            print(item.imageName)
+                        }
                 }
             }
+            .frame(maxWidth: UIScreen.main.bounds.width)
             .background {
                 GeometryReader { proxy in
                     let offset: CGFloat = proxy.frame(in: .named("ScrollView")).origin.y
@@ -49,6 +65,7 @@ struct SizeCompareView: View {
             }
         }
         .coordinateSpace(name: "ScrollView")
+        .background(Color.brown)
     }
 }
 
@@ -58,6 +75,10 @@ struct ScrollOffsetKey: PreferenceKey {
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value += nextValue()
     }
+}
+
+extension CGFloat {
+    static let one: Self = 1
 }
 
 //struct DetectScrollPosition: View {
